64a07b6e25918f39c9755b9e583810a37f965b08
telmen <flyinvoke@gmail.com>
2021-04-19 11:50:25 +0800

    Initial commit

---
+++ api/tweet.js
@@ -0,0 +1,9 @@
+const apiUrl = 'https://ikon.mn/api/json/vaccine';
+
+module.exports = (req, res) => {
+  res.json({
+    body: req.body,
+    query: req.query,
+    cookies: req.cookies,
+  });
+};
